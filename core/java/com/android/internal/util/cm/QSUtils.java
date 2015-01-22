0f86519d855f02b85d5bfa066913c3ebc33326f4
Adnan Begovic <adnan@cyngn.com>
2015-01-21 18:54:39 -0800

    SystemUI: Create LteTile
    Change-Id: Ic193279f930af54d5754358c7e4c97b6b07d2010
    
    Conflicts:
    	core/java/com/android/internal/util/cm/QSConstants.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java

--- core/java/com/android/internal/util/cm/QSUtils.java
+++ core/java/com/android/internal/util/cm/QSUtils.java
@@ -27,6 +27,7 @@ import android.net.ConnectivityManager;
 import android.nfc.NfcAdapter;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
+import com.android.internal.telephony.PhoneConstants;
 
 import java.util.Iterator;
 import java.util.List;
@@ -63,13 +64,7 @@ public class QSUtils {
                 switch (tileKey) {
                     case QSConstants.TILE_CELLULAR:
                     case QSConstants.TILE_HOTSPOT:
-                    case QSConstants.TILE_DATA:
-                    case QSConstants.TILE_ROAMING:
-                    case QSConstants.TILE_APN:
                         removeTile = !deviceSupportsMobile;
-                        break;
-                    case QSConstants.TILE_DDS:
-                        removeTile = !deviceSupportsDdsSupported(context);
                         break;
                     case QSConstants.TILE_FLASHLIGHT:
                         removeTile = !deviceSupportsFlashLight(context);
@@ -94,11 +89,11 @@ public class QSUtils {
         }
     }
 
-    private static boolean deviceSupportsDdsSupported(Context context) {
-        TelephonyManager tm = (TelephonyManager)
-                context.getSystemService(Context.TELEPHONY_SERVICE);
-        return tm.isMultiSimEnabled()
-                && tm.getMultiSimConfiguration() == TelephonyManager.MultiSimVariants.DSDA;
+    public static boolean deviceSupportsLte(Context ctx) {
+        final TelephonyManager tm = (TelephonyManager)
+                ctx.getSystemService(Context.TELEPHONY_SERVICE);
+        return (tm.getLteOnCdmaMode() == PhoneConstants.LTE_ON_CDMA_TRUE)
+                || tm.getLteOnGsmMode() != 0;
     }
 
     public static boolean deviceSupportsMobileData(Context ctx) {
