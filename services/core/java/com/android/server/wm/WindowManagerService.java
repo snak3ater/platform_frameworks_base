20ea1bb38bd3464e86d9d8234cc9f31ff6d328f0
Andy Mast <andy@cyngn.com>
2015-05-16 13:23:13 -0400

    Themes: Fix Reboot Background
    Adapted from commit:
    
    Themes: Fix GlobalAction Background
    Author: Andy Mast
    https://github.com/CyanogenMod/android_frameworks_base/commit/7a8badc2372e6d56d7bdee5057bf4206a2eadd1b

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -940,7 +940,7 @@ public class WindowManagerService extends IWindowManager.Stub
             SurfaceControl.closeTransaction();
         }
 
-        ThemeUtils.registerThemeChangeReceiver(context, mThemeChangeReceiver);
+        ThemeUtils.registerThemeChangeReceiver(mContext, mThemeChangeReceiver);
         updateCircularDisplayMaskIfNeeded();
         showEmulatorDisplayOverlayIfNeeded();
     }
@@ -948,6 +948,7 @@ public class WindowManagerService extends IWindowManager.Stub
     private Context getUiContext() {
         if (mUiContext == null) {
             mUiContext = ThemeUtils.createUiContext(mContext);
+            mUiContext.setTheme(android.R.style.Theme_DeviceDefault_Light_DarkActionBar);
         }
         return mUiContext != null ? mUiContext : mContext;
     }
