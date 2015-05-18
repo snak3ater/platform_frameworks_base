4aa24400adfa9d95a7714bbe28d761cbd4397597
snak3ater <saeedyasir91@gmail.com>
2015-05-18 18:07:40 +0500

    Rework Power menu
    * Add missing code for RebootAction
    
    https://github.com/camcory/platform_frameworks_base/commit/4a39267fc5fac5c7c18e1dba7c9c0a7ae60f9cbd

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -5701,6 +5701,12 @@ public class WindowManagerService extends IWindowManager.Stub
     @Override
     public void rebootSafeMode(boolean confirm) {
         ShutdownThread.rebootSafeMode(mContext, confirm);
+
+    // Called by window manager policy.  Not exposed externally.
+    @Override
+    public void reboot() {
+        ShutdownThread.reboot(getUiContext(), null, true);
+    }
     }
 
     public void setCurrentProfileIds(final int[] currentProfileIds) {
