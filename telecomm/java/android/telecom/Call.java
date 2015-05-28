cc054e6178598e68a05af24009092e8783c1e652
snak3ater <saeedyasir91@gmail.com>
2015-05-28 17:39:17 +0500

    Fix make updateapi for call recording

--- telecomm/java/android/telecom/Call.java
+++ telecomm/java/android/telecom/Call.java
@@ -352,6 +352,7 @@ public final class Call {
 
         /**
          * @return the time the Call object was created
+         * @hide
          */
         public long getCreateTimeMillis() {
             return mCreateTimeMillis;
