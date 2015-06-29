59bbc6684afd01404482e12689d15eb6936e9649
snak3ater <saeedyasir91@gmail.com>
2015-06-30 04:48:08 +0500

    Revert "Show reboot dialog using themed resources"
    This reverts commit 75152ad9e91ca814185a238d71ca1dec5ba773ba.

--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -26,7 +26,6 @@ import android.app.KeyguardManager;
 import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.IBluetoothManager;
-import android.content.pm.ThemeUtils;
 import android.media.AudioAttributes;
 import android.nfc.NfcAdapter;
 import android.nfc.INfcAdapter;
@@ -127,6 +126,7 @@ public final class ShutdownThread extends Thread {
     private static AlertDialog sConfirmDialog;
 
     private static AudioManager mAudioManager;
+    
     private ShutdownThread() {
     }
  
@@ -198,13 +198,12 @@ public final class ShutdownThread extends Thread {
         if (confirm) {
             final CloseDialogReceiver closer = new CloseDialogReceiver(context);
             final boolean advancedReboot = isAdvancedRebootPossible(context);
-            final Context uiContext = getUiContext(context);
 
             if (sConfirmDialog != null) {
                 sConfirmDialog.dismiss();
                 sConfirmDialog = null;
             }
-            AlertDialog.Builder confirmDialogBuilder = new AlertDialog.Builder(uiContext)
+            AlertDialog.Builder confirmDialogBuilder = new AlertDialog.Builder(context)
                     .setTitle(mRebootSafeMode
                             ? com.android.internal.R.string.reboot_safemode_title
                             : showRebootOption
@@ -785,13 +784,4 @@ public final class ShutdownThread extends Thread {
             }
         }
     };
-
-    private static Context getUiContext(Context context) {
-        Context uiContext = null;
-        if (context != null) {
-            uiContext = ThemeUtils.createUiContext(context);
-            uiContext.setTheme(android.R.style.Theme_DeviceDefault_Light_DarkActionBar);
-        }
-        return uiContext != null ? uiContext : context;
-    }
 }
