781450cc132b15fbe29c6a10e85a328d6015c823
Roman Birg <roman@cyngn.com>
2015-09-11 10:49:23 -0700

    SystemUI: add missing super viewgroup method calls
    Change-Id: I5245b03090a1ca06606ba8ee3d132124b4e42a51
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/HeadsUpNotificationView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/HeadsUpNotificationView.java
@@ -254,6 +254,7 @@ public class HeadsUpNotificationView extends FrameLayout implements SwipeHelper.
 
     @Override
     public void onAttachedToWindow() {
+        super.onAttachedToWindow();
         final ViewConfiguration viewConfiguration = ViewConfiguration.get(getContext());
         float touchSlop = viewConfiguration.getScaledTouchSlop();
         mSwipeHelper = new SwipeHelper(SwipeHelper.X, this, getContext());
@@ -296,6 +297,7 @@ public class HeadsUpNotificationView extends FrameLayout implements SwipeHelper.
 
     @Override
     protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
         mContext.getContentResolver().unregisterContentObserver(mSettingsObserver);
     }
 
