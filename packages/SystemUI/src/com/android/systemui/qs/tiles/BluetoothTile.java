33f0dcd4264ad3f16f43036c3ed0b020ba8c6628
d34d <clark@cyngn.com>
2015-09-17 13:54:11 -0700

    QS: Update visibility of items in Bluetooth/WiFi tiles
    Follow up to d67c242218a585f66e84371304f17ae64affedb9
    
    Change-Id: I4e78a0e884116d8e64360bf7fda3f1451c322d31
    TICKET: CYNGNOS-1023

--- packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/BluetoothTile.java
@@ -190,6 +190,7 @@ public class BluetoothTile extends QSTile<QSTile.BooleanState>  {
         public void setToggleState(boolean state) {
             mController.setBluetoothEnabled(state);
             fireToggleStateChanged(state);
+            setItemsVisible(state);
         }
 
         @Override
